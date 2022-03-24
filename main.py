ccca700c69ef1ce301b77348fea94458da7eaa73
Andrew McGrail <andrewjerrismcgrail@gmail.com>
2022-03-23 22:17:15 -0400

    More config options added.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from asyncio import sleep
 import praw
 import prawcore
 from discord import SyncWebhook
@@ -14,15 +15,31 @@ CACHE_FILE = "cache.txt"
 
 
 class CrossPoster:
-    def __init__(self, filename: str, webhook_urls: list, username: str, avatar_url: str):
+    def __init__(
+        self, cache_filename: str, webhook_urls: list, username: str, avatar_url: str
+    ):
+        """Initializes cross poster object.
+
+        Args:
+            cache_filename (str): filename of the cache
+            webhook_urls (list): a list of the webhook urls to use
+            username (str): the username the bot should use
+            avatar_url (str): the url of the avatar the bot should use
+        """
         self.cache: dict = {}
         self.avatar_url = avatar_url
         self.username = username
         self.webhook_urls = webhook_urls
-        self.filename = filename
-        self.load_cache(self.filename)
+        self.cache_filename = cache_filename
+        self.load_cache(self.cache_filename)
 
     def load_cache(self, filename: str):
+        """
+        Loads the cache from a file into memory (self.cache)
+
+        Args:
+            filename (str): the filename to be read from
+        """
         if not os.path.exists(filename):
             with open(filename, "w") as fp:
                 pass
@@ -34,21 +51,34 @@ class CrossPoster:
                 self.cache[line] = ""
 
     def update_cache(self, id: str):
-        if not os.path.exists(self.filename):
-            with open(self.filename, "w") as fp:
+        """
+        Updates both the in memory cache, and this object's given cache filename.
+
+        Args:
+            id (str): the id of the post to update the cache with
+        """
+        if not os.path.exists(self.cache_filename):
+            with open(self.cache_filename, "w") as fp:
                 pass
 
         # no dupe
         if id in self.cache:
             return
 
-        with open(self.filename, "a+") as fp:
+        with open(self.cache_filename, "a+") as fp:
             fp.write(id + "\n")
 
         self.cache[id] = ""
         print("Cache updated with id: ", id)
 
     def post_submission(self, submission, reddit_config_url: str):
+        """
+        Posts a submission from PRAW to all webhook urls this crossposter has.
+
+        Args:
+            submission (submission): the reddit submission to be cross posted to discord.
+            reddit_config_url (str): the reddit url to be used, should be from the PRAW reddit object.
+        """
         with requests.Session() as session:
             for webhook_url in self.webhook_urls:
                 webhook = SyncWebhook.from_url(
@@ -58,10 +88,14 @@ class CrossPoster:
 
                 submission_url = reddit_config_url + submission.permalink
                 post_url = submission.url
-                if "crosspost_parent" in vars(submission) and not r'redd.it' in submission.url and not r'http' in submission.url:
+                if (
+                    "crosspost_parent" in vars(submission)
+                    and not r"redd.it" in submission.url
+                    and not r"http" in submission.url
+                ):
                     print("CROSSPOSTED!")
                     post_url = reddit_config_url + submission.url
-                
+
                 if submission_url == post_url:
                     post_url = ""
 
@@ -80,7 +114,26 @@ class CrossPoster:
                 self.update_cache(submission.id)
 
 
-def main(reddit: praw.Reddit, x_poster: CrossPoster, subreddit_name: str, post_limit: int):
+def main(
+    reddit: praw.Reddit,
+    x_poster: CrossPoster,
+    subreddit_name: str,
+    post_limit: int,
+    wait_period: int,
+):
+    """
+    Main function.
+
+    Args:
+        reddit (praw.Reddit): reddit object from praw to use for submissions
+        x_poster (CrossPoster): cross poster object to use for cross posting to Discord
+        subreddit_name (str): the name of the subreddit to cross post to
+        post_limit (int): how many posts can be gathered from one call of subreddit.new
+        wait_period (int): how many minutes should pass before the post is cross posted (usually to prevent spam)
+
+    Returns:
+        CrossPoster: returns the possibly changed cross poster object
+    """
     subreddit = reddit.subreddit(subreddit_name)
     # get in reverse order to post oldest to newest
     submissions = [submission for submission in subreddit.new(limit=post_limit)][::-1]
@@ -89,7 +142,7 @@ def main(reddit: praw.Reddit, x_poster: CrossPoster, subreddit_name: str, post_l
             time_passed = int(
                 (datetime.timestamp(datetime.now()) - submission.created_utc) / 60
             )
-            if time_passed > 5:
+            if wait_period > 5:
                 try:
                     x_poster.post_submission(submission, reddit.config.reddit_url)
                 except RuntimeError as e:
@@ -114,9 +167,19 @@ if __name__ == "__main__":
         for webhook_url in config["xposter"]["webhook_urls"].split(",")
     ]
     reddit = praw.Reddit("xpost_bot", user_agent="xpost_bot v0.1")
-    x_poster = CrossPoster(CACHE_FILE, webhook_urls, config["xposter"]["username"], config["xposter"]["avatar"])
+    x_poster = CrossPoster(
+        CACHE_FILE,
+        webhook_urls,
+        config["xposter"]["username"],
+        config["xposter"]["avatar"],
+    )
     subreddit_name = config["xposter"]["subreddit"]
+    
     post_limit = int(config["xposter"]["post_limit"])
+    #wait period is in minutes
+    wait_period = int(config["xposter"]["wait_period"])
+    #sleep time is in minutes
+    sleep_time = int(config["xposter"]["sleep_time"])
     prev_cache_count = 0
     while True:
         num_files = len(x_poster.cache) - prev_cache_count
@@ -125,6 +188,5 @@ if __name__ == "__main__":
             print("NUMBER OF FILES LOADED: ", num_files)
 
         prev_cache_count = len(x_poster.cache)
-        x_poster = main(reddit, x_poster, subreddit_name, post_limit)
-        # 3 minutes * 60 for 180 seconds
-        time.sleep(3 * 60)
+        x_poster = main(reddit, x_poster, subreddit_name, post_limit, wait_period)
+        time.sleep(sleep_time * 60)
